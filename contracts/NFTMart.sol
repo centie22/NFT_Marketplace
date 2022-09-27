5e400aa2a9982966a4f36666d155d1aaa3904fff
centie22 <vicentiapeace@gmail.com>
2022-09-27 00:38:20 -0400

    added function

--- contracts/NFTMart.sol
+++ contracts/NFTMart.sol
@@ -1,11 +1,13 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 
 contract NFTMart is ERC721URIStorage, ReentrancyGuard{
     using Counters for Counters.Counter;
@@ -13,12 +15,15 @@ contract NFTMart is ERC721URIStorage, ReentrancyGuard{
     Counters.Counter private _itemsSold;
 
     address payable owner;
+    address celoToken = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
-    constructor () ERC721("NFTMart", "NftM"){
+    constructor () ERC721("NFTMart", "NftM") {
         owner = payable(msg.sender);
     }
 
-uint listingPrice = 0.02 ether;
+uint listingPrice;
+bool listPrice;
+
 
 struct listedTokens {
     uint tokenId;
@@ -30,10 +35,39 @@ struct listedTokens {
 /*Mapping tokenId to Listed NFTs*/
 mapping (uint => listedTokens) private listedTokentrack;
 
+/*Event for NFT listing */
+event NFTlisted(
+    uint indexed tokenId,
+    address martOwner,
+    address NFTowner,
+    uint tokenPrice,
+    bool listed
+
+);
+/*Modifier set to restrict access to certain functions */
+modifier onlyOwner () {
+     require(msg.sender == owner, "Only owner can update Listing price");
+     _;
+}
 /*Function set for only contract owner to update listing price*/ 
-function updateListingPrice(uint _listingPrice) public payable {
-    require(msg.sender == owner, "Only owner can update Listing price");
+function updateListingPrice(uint _listingPrice) public payable onlyOwner{
     _listingPrice = listingPrice;
+    listPrice = true;
+}
+
+function showContractTokenBal () public view returns(uint) {
+    uint bal = IERC20(celoToken).balanceOf(address(this));
+    return bal;
+}
+/*Function set for seller to deposit celoUSD for listing price */
+function depositCelo () internal {
+    IERC20(celoToken).transferFrom(msg.sender, address(this), listingPrice );
+} 
+
+/*Function set for owner to withdraw cUSD from the contract */
+function withdrawCelo (uint amount, address to) external onlyOwner{
+    require(to != address(0), "You can't withdraw to this address"); 
+    IERC20(celoToken).transferFrom(address(this), to, amount);
 }
 /*Function set for users to view cost of listing on the marketplace*/
 function seeListingPrice() public view returns(uint){
@@ -60,7 +94,9 @@ function currentListedToken() public view returns(listedTokens memory){
 
 /*Function to create token/list a token on the marketplace */
 function createToken(string memory tokenURI, uint tokenPrice) public payable nonReentrant returns(uint) {
-    require(msg.value == listingPrice, "Price less then listing price");
+    require(listPrice == true, "You cannot list yet");
+    depositCelo();
+    require(tokenPrice >= listingPrice, "Price less then listing price");
     require(tokenPrice >0, "Nft price cannot be 0 ether");
 
     /*Set new token id by increasing the previous id  */
@@ -83,19 +119,40 @@ function createlistedToken(uint tokenId, uint tokenPrice) private{
     );
     /*After creating the object, the token is transferred to the Marketplace contract */
     _transfer(msg.sender, address(this), tokenId);
-}
 
+/*emitting the listing event */
+emit NFTlisted(
+    tokenId, 
+address(this),
+ msg.sender, 
+ tokenPrice, 
+ true
+ );
+}
+/*Function set to return all NFTs up for sale on this marketplace */
+function viewAllNFTs() public view returns(listedTokens[] memory){
+    uint noOfNfts = _tokenId.current();
+    listedTokens[] memory NFTs = new listedTokens[](noOfNfts);
+    uint index = 0;
+    uint id;
+
+    for (uint i = 0; i < noOfNfts; i++) {
+        id = i + 1;
+        listedTokens storage currentNFT = listedTokentrack[id];
+        NFTs[index] = currentNFT;
+        index += 1;
+    }
+    return NFTs;
+}
+/*Function for buying listed NFTs */
 function buyNFT(uint256 NFTid) public payable nonReentrant{
     /*setting variables for the NFT price and the seeler of the NFT */
     uint nftPrice = listedTokentrack[NFTid].tokenPrice;
-    address payable seller = listedTokentrack[NFTid].NFTowner;
-    require (msg.value == nftPrice, "Not enough Ether to pay for this NFT");
-/*Transfer the listing price to the mart owner */
-    payable(owner).transfer(listingPrice);
+    address seller = listedTokentrack[NFTid].NFTowner;
 
     /*Transferring the value paid for the NFT to the seller */
-    payable(seller).transfer(msg.value);
-
+    IERC20(celoToken).transferFrom(msg.sender, seller, nftPrice);
+    
     /*Approve the Marketplace to send the NFT to the buyer in order to transfer the token to the buyer */
     approve(address(this), NFTid);
 
@@ -116,8 +173,9 @@ function buyNFT(uint256 NFTid) public payable nonReentrant{
 /*If the buyer of the token wishes to re-list the token on the marketplacd for sale, then 
 buyer can call this function */
 function relistForSale (uint NFTid, uint price) public payable {
-    require (msg.value == listingPrice, "Listing price requirement not met");
-    require(price > 0, "Price of NFT cannot be 0 ether!");
+    depositCelo();
+    // require (IERC20(celoToken) == listingPrice, "Listing price requirement not met");
+    require(price > 0, "Price of NFT cannot be 0 celo!");
    bool  list = listedTokentrack[NFTid].listed;
     price = listedTokentrack[NFTid].tokenPrice;
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-require('dotenv').config();
-//import "@nomiclabs/hardhat-ethers";
-type HttpNetworkAccountUserConfig = any;
-const {API_URL, PRIVATE_KEY} = process.env;
-const config: HardhatUserConfig = {
+require("dotenv").config({ path: ".env" });
+module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "goerli",
-  networks:{
-    hardhat:{},
-    goerli:{
-      url: API_URL,
-      accounts:[PRIVATE_KEY] as HttpNetworkAccountUserConfig | undefined,
-    }
-  }
-};
+  networks: {
+    alfajores: {
+      url: process.env.API_URL,
+      accounts: {
+        mnemonic: process.env.MNEMONIC_KEYS, // line 25
+        path: "m/44'/52752'/0'/0",
+      chainId: 44787,
+    },
+  },
+}
+}
+// import "@nomiclabs/hardhat-ethers";
+//  type HttpNetworkAccountUserConfig = any;
+// const {API_URL, PRIVATE_KEY} = process.env;
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.9",
+//   defaultNetwork: "goerli",
+//   networks:{
+//     hardhat:{},
+//     goerli:{
+//       url: API_URL,
+//       accounts:[PRIVATE_KEY] as HttpNetworkAccountUserConfig | undefined,
+//     }
+//   }
+// };
 
-export default config;
+//export default config;
